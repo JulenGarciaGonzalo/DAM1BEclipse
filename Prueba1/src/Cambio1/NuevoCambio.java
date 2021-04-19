56da7bd0b6286d7f4b4915f220380670d202ebb1
Julen <julen.gargon@educa.jcyl.es>
2021-04-19 16:37:53 +0200

    Validando cambio1 en cambio1

--- Prueba1/src/Cambio1/NuevoCambio.java
+++ Prueba1/src/Cambio1/NuevoCambio.java
@@ -2,6 +2,7 @@ package Cambio1;
 
 public class NuevoCambio {
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		System.out.println("Hola cambio1");
+		System.out.println("Hola otra vez cambio1");
 	}
 }
