46453f87d88b16b505ed78885118cc3d4d81969d
Julen <julen.gargon@educa.jcyl.es>
2021-04-19 16:34:44 +0200

    Validación en cambio1

---
+++ Prueba1/src/Cambio1/NuevoCambio.java
@@ -0,0 +1,7 @@
+package Cambio1;
+
+public class NuevoCambio {
+	public static void main(String[] args) {
+		System.out.println("Hola");
+	}
+}
--- Prueba1/src/Prueba1/Principal.java
+++ Prueba1/src/Prueba1/Principal.java
@@ -3,5 +3,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo. Probando Git en eclipse");
 		System.out.println("Hola otra vez");
+		System.out.println("Hola mundo desde la rama cambio1");
 	}
 }
